71c61e02b37b83889f3c6331e3e60a7cecbe9302
roh7itt <krohit.info@gmail.com>
2026-02-18 11:52:11 +0530

    2

---
+++ src/PW/MultiplicationOfArray.java
@@ -0,0 +1,64 @@
+package PW;
+
+
+import java.util.Scanner;
+
+
+public class MultiplicationOfArray {
+    static void printMatrix(int[][] matrix){
+        for(int i =0; i<matrix.length; i++){
+            for (int j=0; j<matrix.length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void multiply(int[][] a, int r1, int r2, int[][] b, int c1, int c2) {
+        if (c1 != r2) {
+            System.out.println("Multiplication not possible");
+            return;
+        }
+        int[][] mul = new int[r1][c2];
+        for (int i = 0; i < r1; i++) {//Row number
+            for (int j = 0; j < c2; j++) {//Column number
+                for (int k = 0; k < c1; k++) {
+                    mul[i][j] += (a[i][k] * b[k][j]);
+                }
+            }
+        }
+        System.out.println("Multiplication of 2 matrices");
+        printMatrix(mul);
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows and columns of matrix 1");
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        int[][] a = new int[r1][c1];
+        System.out.println("Enter matrix values");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter number of rows and columns of the Matrix 2");
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+        int[][] b = new int[r2][c2];
+        System.out.println("Enter matrix values");
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix 1");
+        printMatrix(a);
+        System.out.println("Matrix 2");
+        printMatrix(b);
+
+        multiply(a, r1, c1, b, r2, c2);
+    }
+
+}
---
+++ src/PW/Transpose.java
@@ -0,0 +1,44 @@
+package PW;
+
+import java.util.Scanner;
+
+public class Transpose {
+    static void printMatrix(int[][] matrix){
+        for(int i =0; i<matrix.length; i++){
+            for (int j=0; j<matrix.length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[][]  findTranspose(int[][] matrix, int r, int c){
+        int[][] ans = new int[c][r];
+         for (int i = 0; i<c; i++){
+             for (int j =0; j<r; j++){
+                 ans[i][j]=matrix[j][i];
+             }
+         }
+         return ans;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows and columns of matrix");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        System.out.println("Enter matrix values");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Input matrix");
+        printMatrix(matrix);
+
+        System.out.println("Transpose of the matrix");
+        int[][] ans = findTranspose(matrix, r, c);
+
+    }
+}
